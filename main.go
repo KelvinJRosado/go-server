6003f16af9007a2f7774d908cd59f1c8a8298867
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-17 14:11:33 -0400

    feat: Add basic HTTP server with logging

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("go-server")
+	// Create inputs for server
+	serverHandler := http.NewServeMux()
+	serverAddress := ":8080"
+
+	// Create server
+	server := http.Server{
+		Handler: serverHandler,
+		Addr:    serverAddress,
+	}
+
+	// Start server and log any failures
+	slog.Info("Starting server", "address", serverAddress)
+	err := server.ListenAndServe()
+	if err != nil {
+		slog.Error("Server Error", "error", err)
+		os.Exit(1)
+	}
 }
