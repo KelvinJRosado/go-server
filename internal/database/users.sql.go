0d6b10f65c25a94e1cccb62f4527d448140402c9
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-23 13:36:39 -0400

    feat: Add hashed_password field to User model

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -11,21 +11,33 @@ import (
 
 const createUser = `-- name: CreateUser :one
 INSERT INTO
-  users (id, created_at, updated_at, email)
+  users (
+    id,
+    created_at,
+    updated_at,
+    email,
+    hashed_password
+  )
 VALUES
-  (gen_random_uuid(), NOW(), NOW(), $1)
+  (gen_random_uuid(), NOW(), NOW(), $1, $2)
 RETURNING
-  id, created_at, updated_at, email
+  id, created_at, updated_at, email, hashed_password
 `
 
-func (q *Queries) CreateUser(ctx context.Context, email string) (User, error) {
-	row := q.db.QueryRowContext(ctx, createUser, email)
+type CreateUserParams struct {
+	Email          string `json:"email"`
+	HashedPassword string `json:"hashed_password"`
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, createUser, arg.Email, arg.HashedPassword)
 	var i User
 	err := row.Scan(
 		&i.ID,
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Email,
+		&i.HashedPassword,
 	)
 	return i, err
 }
